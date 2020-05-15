457e5204fb4cd9fede726e025f56a6bdcca347ff
Florian Lahitte <lahitte.florian@gmail.com>
2020-05-15 15:53:55 -0400

    add react navigation v5

--- App.js
+++ App.js
@@ -1,12 +1,71 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Button } from 'react-native-elements';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createNativeStackNavigator } from 'react-native-screens/native-stack';
+import { enableScreens } from 'react-native-screens';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+import AuthScreen from './screens/AuthScreen';
+import WelcomeScreen from './screens/WelcomeScreen';
+import DeckScreen from './screens/DeckScreen';
+import MapScreen from './screens/MapScreen';
+import ReviewScreen from './screens/ReviewScreen';
+import SettingScreen from './screens/SettingScreen';
+enableScreens();
+class App extends React.Component {
+  render() {
+    const Tab = createBottomTabNavigator();
+    const Stack = createNativeStackNavigator();
+
+    function MyStack({ navigation }) {
+      return (
+        <Stack.Navigator initialRouteName="Review jobs">
+          <Stack.Screen
+            options={{
+              headerRight: () => (
+                <Button
+                  onPress={() => navigation.navigate('Setting')}
+                  title="Setting"
+                />
+              ),
+              headerStyle: {
+                backgroundColor: '#f4511e',
+                height: '120px',
+              },
+              headerTintColor: '#fff',
+              headerTitleStyle: {
+                fontWeight: 'bold',
+              },
+            }}
+            name="Review"
+            component={ReviewScreen}
+          />
+          <Stack.Screen name="Setting" component={SettingScreen} />
+        </Stack.Navigator>
+      );
+    }
+
+    function main() {
+      return (
+        <Tab.Navigator>
+          <Tab.Screen name="Deck" component={DeckScreen} />
+          <Tab.Screen name="Map" component={MapScreen} />
+          <Tab.Screen name="Review" component={MyStack} />
+        </Tab.Navigator>
+      );
+    }
+
+    return (
+      <NavigationContainer>
+        <Tab.Navigator>
+          <Tab.Screen name="Home" component={WelcomeScreen} />
+          <Tab.Screen name="Settings" component={AuthScreen} />
+          <Tab.Screen name="Main" component={main} />
+        </Tab.Navigator>
+      </NavigationContainer>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -17,3 +76,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default App;
---
+++ screens/ReviewScreen.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class ReviewScreen extends Component {
+  render() {
+    return (
+      <View>
+        {/* <Button
+          title="Go to setting"
+          onPress={() => this.props.navigation.navigate('Setting')}
+        /> */}
+        <Text>ReviewScreen</Text>
+        <Text>ReviewScreen</Text>
+        <Text>ReviewScreen</Text>
+        <Text>ReviewScreen</Text>
+        <Text>ReviewScreen</Text>
+        <Text>ReviewScreen</Text>
+      </View>
+    );
+  }
+}
+
+export default ReviewScreen;

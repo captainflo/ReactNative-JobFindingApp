04079a7629306164544b0dd0c27f4c0aba25231a
Florian Lahitte <lahitte.florian@gmail.com>
2020-05-24 17:12:02 -0400

    add viewmap and list of job

---
+++ reducers/likes_reducer.js
@@ -0,0 +1,19 @@
+import _ from 'lodash';
+import { LIKE_JOB, CLEAR_LIKED_JOBS } from '../actions/types';
+import { REHYDRATE } from 'redux-persist/lib/constants';
+
+export default function (state = [], action) {
+  switch (action.type) {
+    case REHYDRATE:
+      return action.payload.likedJob || [];
+    case LIKE_JOB:
+      // only capture one job and not duplicate job
+      // contain new array where the new job ha has liked and push with the other
+      // loadash will take unique job by jobkey
+      return _.unionBy([action.payload, ...state], 'jobkey');
+    case CLEAR_LIKED_JOBS:
+      return [];
+    default:
+      return state;
+  }
+}
--- screens/ReviewScreen.js
+++ screens/ReviewScreen.js
@@ -1,23 +1,67 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, ScrollView, Linking } from 'react-native';
+import { connect } from 'react-redux';
+import { Card, Button } from 'react-native-elements';
+import MapView from 'react-native-maps';
 
 class ReviewScreen extends Component {
+  renderLikedJobs() {
+    return this.props.likedJobs.map((job) => {
+      const initialRegion = {
+        longitude: job.longitude,
+        latitude: job.latitude,
+        // for the zoom
+        latitudeDelta: 0.045,
+        longitudeDelta: 0.02,
+      };
+      return (
+        <Card title={job.jobtitle} key={job.jobkey}>
+          <View style={{ height: 200 }}>
+            <MapView
+              style={{ flex: 1 }}
+              cacheEnabled={Platform.OS === 'android' ? true : false} // it's render as static image
+              scrollEnabled={false} // cannot scroll
+              zoomTapEnabled={false}
+              zoomEnabled={false}
+              zoomControlEnabled={false}
+              initialRegion={initialRegion}
+            />
+          </View>
+          <View style={styles.detailWrapper}>
+            <Text style={styles.italics}>{job.company}</Text>
+            <Text>{job.formattedRelativeTime}</Text>
+          </View>
+          <Button title="Apply Now!" onPress={() => Linking.openURL(job.url)} />
+        </Card>
+      );
+    });
+  }
+
   render() {
     return (
       <View>
-        {/* <Button
-          title="Go to setting"
-          onPress={() => this.props.navigation.navigate('Setting')}
-        /> */}
-        <Text>ReviewScreen</Text>
-        <Text>ReviewScreen</Text>
-        <Text>ReviewScreen</Text>
-        <Text>ReviewScreen</Text>
-        <Text>ReviewScreen</Text>
-        <Text>ReviewScreen</Text>
+        <ScrollView>{this.renderLikedJobs()}</ScrollView>
       </View>
     );
   }
 }
 
-export default ReviewScreen;
+const styles = {
+  detailWrapper: {
+    marginBottom: 10,
+    marginTop: 10,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+  },
+  italics: {
+    fontStyle: 'italic',
+  },
+};
+
+function mapStateToProps(state) {
+  return {
+    likedJobs: state.likedJobs,
+  };
+}
+
+export default connect(mapStateToProps)(ReviewScreen);
